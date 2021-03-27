7a4cf90602b4d31ad2fad038fd0fd6fd76035e67
Felipe <mrchampz@outlook.com>
2021-03-27 03:16:31 -0300

    Trying

--- .docker/mysql/initdb.sql
+++ .docker/mysql/initdb.sql
@@ -1,3 +1,2 @@
 CREATE DATABASE IF NOT EXISTS code_micro_videos;
-DROP TABLE IF EXISTS code_micro_videos_test;
-CREATE DATABASE code_micro_videos_test;
+CREATE DATABASE IF NOT EXISTS code_micro_videos_test;
