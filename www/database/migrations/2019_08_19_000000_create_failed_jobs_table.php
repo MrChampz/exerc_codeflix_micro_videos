f9aaf70121760db861e209a86bdd7d34b26864d7
Felipe <mrchampz@outlook.com>
2021-03-27 03:43:38 -0300

    Trying

--- www/database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ www/database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateFailedJobsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
@@ -23,12 +18,7 @@ class CreateFailedJobsTable extends Migration
             $table->timestamp('failed_at')->useCurrent();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+    
     public function down()
     {
         Schema::dropIfExists('failed_jobs');
